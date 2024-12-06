dd681c3082c3a5818a690a4cab522c33819dd22f
rafi dhana <rafidhn06@email.com>
2024-12-06 23:29:25 +0700

    new file:   include/tabel.h

---
+++ include/tabel.h
@@ -0,0 +1,28 @@
+#ifndef TABEL_H_INCLUDED
+#define TABEL_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <variant>
+
+typedef std::variant<int, double, std::string> sel;
+struct tabel {
+    std::vector<std::string> atribut;
+    std::vector<std::vector<sel>> record;
+};
+
+template <typename... Args>
+void addAtribut(tabel& t, Args... args) {
+    (t.atribut.push_back(args),...);
+}
+
+void addRecord(tabel& t, std::vector<std::vector<sel>> record) {
+    for (unsigned i = 0; i < record.size(); i++) {
+        if (record[i].size() != t.atribut.size()) {
+            throw std::invalid_argument("Panjang record tidak sesuai dengan banyak atribut");
+        }
+        t.record.push_back(record[i]);
+    }
+}
+
+#endif // TABEL_H_INCLUDED
